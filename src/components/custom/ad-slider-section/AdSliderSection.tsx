b96fb46dfe870c834ddb7bca31466043f1532263
gramaphenia <dicoratojuanpablo@gmail.com>
2025-03-09 09:29:22 -0300

    mods-add-slider-tablet-responsive

--- src/components/custom/ad-slider-section/AdSliderSection.tsx
+++ src/components/custom/ad-slider-section/AdSliderSection.tsx
@@ -42,9 +42,8 @@ export function AdSliderSection() {
   };
 
   return (
-    <section className="px-4 md:px-8 lg:my-20 max-w-xs lg:max-w-7xl mx-auto relative mb-10">
+    <section className="px-4 md:px-0 lg:my-20 max-w-xs md:max-w-screen-sm lg:max-w-7xl mx-auto relative mb-10">
       <h2
-        id="card-title"
         className={`${playfair.className} text-3xl sm:text-4xl lg:text-5xl text-center font-normal mb-6 sm:mb-8 lg:mb-10 text-zinc-600 dark:text-zinc-300`}
       >
         Espacio publicitario
@@ -52,8 +51,9 @@ export function AdSliderSection() {
       <div className="w-full max-w-7xl mx-auto overflow-hidden relative">
         <div
           className="flex transition-transform duration-500 ease-in-out"
-          style={{ transform: `translateX(-${currentIndex * (100 / itemsPerPage)}%)` }}
-          aria-live="polite"
+          style={{
+            transform: `translateX(-${currentIndex * (100 / itemsPerPage)}%)`,
+          }}
         >
           {ads.map((ad) => (
             <div
@@ -61,14 +61,13 @@ export function AdSliderSection() {
               className="flex-shrink-0"
               style={{ width: `${100 / itemsPerPage}%`, padding: "0 8px" }}
             >
-              <div className="bg-white dark:bg-gray-800 rounded-lg p-4 text-center border">
+              <div className="bg-white dark:bg-gray-800 rounded-lg p-4 text-center border relative">
                 <div className="relative w-full h-48 sm:h-64 md:h-72 lg:h-80 mb-4 overflow-hidden">
                   <Image
                     src={ad.image}
                     alt={ad.title}
-                    layout="fill"
-                    objectFit="cover"
-                    className="rounded-lg border"
+                    fill
+                    className="rounded-lg border object-cover"
                   />
                 </div>
                 <h3 className="font-semibold text-lg mb-2 text-stone-600 dark:text-stone-200">
@@ -79,32 +78,18 @@ export function AdSliderSection() {
             </div>
           ))}
         </div>
-        <div className="flex justify-center md:hidden space-x-40 mt-4">
-          <button
-            onClick={prevSlide}
-            className="text-orange-400 px-4 py-2 rounded-full bg-orange-200 border border-orange-200/50 transition-all ease-in-out duration-200 "
-            aria-label="Anterior"
-          >
-            &#10094;
-          </button>
-          <button
-            onClick={nextSlide}
-            className="text-orange-400 px-4 py-2 rounded-full bg-orange-200 border border-orange-200/50 transition-all ease-in-out duration-200 "
-            aria-label="Siguiente"
-          >
-            &#10095;
-          </button>
-        </div>
+      </div>
+      <div className="absolute -bottom-14 right-4 flex space-x-2">
         <button
           onClick={prevSlide}
-          className="absolute left-4 top-1/2 transform -translate-y-1/2 text-orange-400 px-4 py-2 rounded-full bg-orange-200 border border-orange-200/50 transition-all ease-in-out duration-200 shadow-md z-10 hidden md:block"
+          className="text-orange-400 px-4 py-2 rounded-md bg-orange-200 border border-orange-200/50 transition-all ease-in-out duration-200"
           aria-label="Anterior"
         >
           &#10094;
         </button>
         <button
           onClick={nextSlide}
-          className="absolute right-4 top-1/2 transform -translate-y-1/2 text-orange-400 px-4 py-2 rounded-full bg-orange-200 border border-orange-200/50 transition-all ease-in-out duration-200 shadow-md z-10 hidden md:block"
+          className="text-orange-400 px-4 py-2 rounded-md bg-orange-200 border border-orange-200/50 transition-all ease-in-out duration-200"
           aria-label="Siguiente"
         >
           &#10095;
