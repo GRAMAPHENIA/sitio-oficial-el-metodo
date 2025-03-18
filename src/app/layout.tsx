20f0e9177da3e708c8ea013adc2bfa3d1ce1a369
gramaphenia <dicoratojuanpablo@gmail.com>
2025-03-17 22:29:13 -0300

    Se resolvio el faltante de 'FooterProps'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
       >
         <Menu />
         {children}
-        <Footer />
+        <Footer currentYear={new Date().getFullYear()} />
       </body>
     </html>
   );
